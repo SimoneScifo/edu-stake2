7deed1602c7cf1313177dc031c05e6bfad000465
SimoneScifo <simone.scifo97@gmail.com>
2024-07-13 16:11:34 +0200

    Fixed header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {RouterModule} from '@angular/router';
 import { LoginDialogComponent } from "./login-dialog/login-dialog.component";
-
+import { MatToolbarModule } from '@angular/material/toolbar';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, RouterModule, LoginDialogComponent],
+  imports: [RouterOutlet, HomeComponent, RouterModule, LoginDialogComponent, MatToolbarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
