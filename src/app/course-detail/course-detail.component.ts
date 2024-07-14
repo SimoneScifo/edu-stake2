33f558d7b433b581f41d26b556c509fbd327ee77
Tonino <Tony@MacBook-Pro-Tonino.local>
2024-07-14 12:37:19 +0200

    start video

--- src/app/course-detail/course-detail.component.ts
+++ src/app/course-detail/course-detail.component.ts
@@ -1,14 +1,22 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, NgZone } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { CourseService } from '../course.service';
 import { Course } from '../course';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { SafeUrlPipeModule } from '../safe-url/safe-url.pipe.module';
-import { MatDialog } from '@angular/material/dialog';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { PaymentPopupComponent } from '../payment-popup/payment-popup.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSliderModule } from '@angular/material/slider';
+import { Inject } from '@angular/core';
+
+declare global {
+  interface Window {
+    onYouTubeIframeAPIReady: any;
+    YT: any;
+  }
+}
 
 @Component({
   selector: 'app-course-detail',
@@ -22,22 +30,32 @@ import { MatSliderModule } from '@angular/material/slider';
     MatSliderModule
   ],
   templateUrl: './course-detail.component.html',
-  styleUrl: './course-detail.component.css',
+  styleUrls: ['./course-detail.component.css'],
 })
 export class CourseDetailComponent implements OnInit {
   route: ActivatedRoute = inject(ActivatedRoute);
   courseService = inject(CourseService);
   course: Course | undefined;
+  ngZone: NgZone = inject(NgZone);
 
   isPaymentConfirmed: boolean = false;
+  isCompletedEnabled: boolean = false;
   paymentError: boolean = false;
   showPaymentModal: boolean = false;
   showStakeProposalModal: boolean = false;
   stakedTokenAmount: number = 0;
+  timeUpdateInterval: any;
+  player: any;
 
+  relatedCourses = [
+    { title: 'Course 1' },
+    { title: 'Course 2' },
+    { title: 'Course 3' },
+    { title: 'Course 4' },
+    { title: 'Course 5' },
+  ];
 
-  constructor(public dialog: MatDialog) {
-  }
+  constructor(public dialog: MatDialog) {}
 
   ngOnInit(): void {
     const courseId = parseInt(this.route.snapshot.params['id'], 10);
@@ -60,8 +78,8 @@ export class CourseDetailComponent implements OnInit {
     });
   }
 
-  processPayment(paymentConfirmationReceived:boolean) {
-    this.isPaymentConfirmed = (paymentConfirmationReceived)
+  processPayment(paymentConfirmationReceived: boolean) {
+    this.isPaymentConfirmed = paymentConfirmationReceived;
   }
 
   handlePaymentConfirmation(success: boolean) {
@@ -79,5 +97,10 @@ export class CourseDetailComponent implements OnInit {
     if (amount > 0) {
       alert(`Staked ${amount} tokens on Solana blockchain!`);
     }
+  }
+
+
+  completeCourse(): void {
+    alert('🎉 Congratulazioni hai completato il corso 🎉');
   }
 }
